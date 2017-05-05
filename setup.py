2bf8133bf0b594991e85cd802b77069604dcba75
William Nielsen <whpn@mailbox.org>
2017-05-05 14:11:03 +0200

    meta: Scripts for installation, add them

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+setup(
+    name='broadbean',
+    version='0.9',
+
+    # We might as well require what we know will work
+    # although older numpy and matplotlib version will probably work too
+    install_requires=['numpy>=1.12.1',
+                      'matplotlib>=2.0.1'],
+
+    author='William H.P. Nielsen',
+    author_email='whpn@mailbox.org',
+
+    description=("Package for easily generating and manipulating signal "
+                 "pulses. Developed for use with qubits in the quantum "
+                 "computing labs of Copenhagen, Delft, and Sydney, but "
+                 "should be generally useable."),
+
+    license='MIT',
+
+    packages=['broadbean.py', 'ripasso.py'],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Students and Scientists',
+        'Programming Language :: Python :: 3.5'
+        ],
+
+    keywords='Pulsebuilding signal processing arbitrary waveforms',
+
+    url='https://github.com/QCoDeS/broadbean'
+    )
