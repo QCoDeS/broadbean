bac32f4e41c3d0827e214c073be58f6130134f5f
William Nielsen <whpn@mailbox.org>
2017-05-17 16:04:40 +0200

    WIP: Add test for Element

---
+++ tests/test_element.py
@@ -0,0 +1,35 @@
+# Test suite for the Element Object of the broadband package
+#
+# We let the test_blueprint.py test the BluePrint and only test that Elements
+# work on the Element level
+
+import pytest
+import broadbean as bb
+
+ramp = bb.PulseAtoms.ramp
+sine = bb.PulseAtoms.sine
+tophat_SR = 2000
+
+
+@pytest.fixture
+def blueprint_tophat():
+    """
+    Return a blueprint consisting of three slopeless ramps forming something
+    similar to a tophat
+    """
+    th = bb.BluePrint()
+    th.insertSegment(0, ramp, args=(0, 0), name='ramp', durs=1)
+    th.insertSegment(1, ramp, args=(1, 1), name='ramp', durs=0.5)
+    th.insertSegment(2, ramp, args=(0, 0), name='ramp', durs=1)
+    th.setSR(tophat_SR)
+
+    return th
+
+##################################################
+# TEST BARE INITIALISATION
+
+
+def test_bare_init(blueprint_tophat):
+    elem = bb.Element()
+    elem.addBluePrint(1, blueprint_tophat)
+    assert list(elem._data.keys()) == [1]
