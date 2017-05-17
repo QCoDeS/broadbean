a08fff5946f5faa5d174cf7536bd4e71e6d299a0
William Nielsen <whpn@mailbox.org>
2017-05-17 11:14:04 +0200

    refactor: Make the test look fancy
    Use fancy decorators to make the simple test look fancy.

--- tests/test_blueprint.py
+++ tests/test_blueprint.py
@@ -9,25 +9,42 @@ def virgin_blueprint():
     """
     return bb.BluePrint()
 
+
 ##################################################
 # TEST BARE INITIALISATION
-
 
 def test_creation(virgin_blueprint):
     assert isinstance(virgin_blueprint, bb.BluePrint)
 
 
-def test_creation_funlist(virgin_blueprint):
-    assert virgin_blueprint._funlist == []
+@pytest.mark.parametrize("attribute, expected", [('_funlist', []),
+                                                 ('_namelist', []),
+                                                 ('_argslist', []),
+                                                 ('_tslist', [])])
+def test_bob(virgin_blueprint, attribute, expected):
+    assert virgin_blueprint.__getattribute__(attribute) == expected
 
 
-def test_creation_nameslist(virgin_blueprint):
-    assert virgin_blueprint._namelist == []
 
 
-def test_creation_argslist(virgin_blueprint):
-    assert virgin_blueprint._argslist == []
+
+# def test_creation_funlist(virgin_blueprint):
+#     assert virgin_blueprint._funlist == []
 
 
-def test_creation_tslist(virgin_blueprint):
-    assert virgin_blueprint._tslist == []
+# def test_creation_nameslist(virgin_blueprint):
+#     assert virgin_blueprint._namelist == []
+
+
+# def test_creation_argslist(virgin_blueprint):
+#     assert virgin_blueprint._argslist == []
+
+
+# def test_creation_tslist(virgin_blueprint):
+#     assert virgin_blueprint._tslist == []
+
+
+#def test_creation_durslist(virgin_blueprint):
+#    assert virgin_blueprint._durslist == []
+
+#def test_creation_marker1(vi
