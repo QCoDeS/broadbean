3fcae36b2c6adcb71efbe120ddbd568e39437248
Dominik Vogel <a-dovoge@microsoft.com>
2018-04-13 11:31:03 +0200

    making duration a special property

--- broadbean/segment_concept.py
+++ broadbean/segment_concept.py
@@ -8,59 +8,76 @@ PropertyDict = Dict[str, Property]
 ContextDict = Dict[str, Number]
 TimeType = np.ndarray
 
+class _Expandable:
+    pass
+
+expandable = _Expandable()
+
 class Segment:
-    def __init__(self, **properties: PropertyDict):
-        if 'duration' not in properties:
-            raise ValueError('You must specify a duration for a segment. It may be "None" if it is dynamically determined.')
+    def __init__(self, duration, **properties: PropertyDict):
+        properties['duration'] = duration
         self._properties = properties
 
 
     def forge(self,
               SR: Number,
-              **context_and_values: ContextDict) -> np.ndarray:
+              duration = None,
+              **context: ContextDict) -> np.ndarray:
         raise NotImplementedError
 
-    def get_property(self,
-                     name: str,
-                     **context_and_values: ContextDict) -> Number:
+    def get(self,
+            name: str,
+            **context: ContextDict) -> Number:
         # it might be prone to errors to put context and values together into one dict
-        k, v = name, self._properties[name]
-        if v is None:
-            # item is defined as by_value
-            if k not in context_and_values:
-                raise ValueError('Property {}:{} has to be supplied as a value.'.format(k, v))
-            return context_and_values[k]
-        elif isinstance(v, str):  # add possibility for escaping strings here
-            # item is defined as by_context
-            # make sure it is not also defined as value
-            if k in context_and_values:
-                raise ValueError('Property {}:{} is already speciefied in the context and cannot also be specified as a value.'.format(k, v))
-            return context_and_values[v]
+        value_or_symbol = self._properties[name]
+        if isinstance(value_or_symbol, str):  # add possibility for escaping strings here
+            # is symbol
+            return context[value_or_symbol]
         else:
-            # item has a fixed value
-            return v
+            # is value
+            return value_or_symbol
 
     def get_all_properties(self,
-                       **context_and_values: ContextDict) -> Dict[str, Number]:
-        return {k:self.get_property(k, **context_and_values) for k,v in self._properties.items()}
-        
+                           **context: ContextDict) -> Dict[str, Number]:
+        return {k:self.get(k, **context) for k,v in self._properties.items()}
+
+    @property
+    def is_expandable(self):
+        return self._properties['duration'] == expandable
+
+    def _safe_get_duration(self, duration, **context):
+        if self.is_expandable:
+            # duration must be supplied explicitly
+            if duration is None:
+                raise ValueError('Must supply duration')
+            return duration
+        else:
+            # duration fixed by segment
+            if duration is not None:
+                raise ValueError('Duration cannot be overridden.')
+            own_duration = self.get('duration', **context)
+            if own_duration is None:
+                raise ValueError('Must supply duration')
+            return self.get('duration', **context)
+
 
 
 class FunctionSegment(Segment):
     def __init__(self,
                  function: callable,
-                 duration: Number,
+                 duration: Number=None,
                  **function_arguments: PropertyDict) -> None:
         # check if compatible with function footprint and has a duration
-        super().__init__(**function_arguments, duration=duration)
+        if duration is None:
+            raise ValueError('Must supply a duration to a FunctionSegment')
+        super().__init__(duration=duration, **function_arguments)
         self._function = function
 
     def forge(self,
               SR: Number,
-              **context_and_values: ContextDict) -> np.ndarray:
-        # retrieve values valid in context
-        properties = deepcopy(self.get_all_properties(**context_and_values))
-        duration = properties.pop('duration')
+              duration=None,
+              **context: ContextDict) -> np.ndarray:
+        duration = self._safe_get_duration(duration, **context)
 
         # check minimum length
         int_dur = int(duration*SR)
@@ -70,52 +87,64 @@ class FunctionSegment(Segment):
                              f'{int_dur}')
         # create time array
         time_array = np.linspace(0, duration, int_dur, endpoint=False)
-        properties.update({'time': time_array})
-
-        return self._function(**properties)
+        kwargs = self.get_all_properties(**context)
+        kwargs.pop('duration')
+        return self._function(time=time_array,
+                              **kwargs)
 
 
 class GroupSegment(Segment):
 # TODO: prevent recursive nesting of segments
     def __init__(self,
                  *segments: List['Segment'],
+                 duration = None,
                  **properties: PropertyDict) -> None:
-        # propterties is basically duration
-        super().__init__(**properties)
+        super().__init__(duration=duration, **properties)
         self._segments = segments
 
-    def get_property(self,
-                     name: str,
-                     **context_and_values: ContextDict) -> Number:
+    def get(self,
+            name: str,
+            **context: ContextDict) -> Number:
         if name == 'duration':
-            n_expandable_children = sum(1 for s in self._segments if s._properties[name] is None)
+            n_expandable_children = sum(1 for s in self._segments if s.is_expandable)
             if n_expandable_children == 0:
                 # duration fixed by children
-                duration = sum(s.get_property(name, **context_and_values) for s in self._segments)
-                return super().get_property(name, **context_and_values, duration=duration)
+                if self._properties['duration'] is not None:
+                    raise RuntimeError('duration has been specified for group, but all child elements also have a fixed duration.')
+                return sum(s.get(name, **context) for s in self._segments)
             elif n_expandable_children > 1:
                 # this case is not supported
                 raise RuntimeError('only one child segment can be expandable')
             else:
-                # duration fixed by group segment
-                return super().get_property(name, **context_and_values)
+                # has exactly one expandable child
+                # duration may be fixed, expandable, or symbol
+                return super().get(name, **context)
+        else:
+            return super().get(name, **context)
 
     def forge(self,
               SR: Number,
-              **context_and_values: ContextDict) -> np.ndarray:
+              duration=None,
+              **context: ContextDict) -> np.ndarray:
         # getting here means that there is exactly one or none child segment with undetermined duration
 
+        # checks if duration is supplyable by an argument, fails if not
+        own_duration = self._safe_get_duration(duration, **context)
+    
+        # from IPython.core.debugger import set_trace
+        # set_trace()
+
         # TODO: Make this faster once everything works
-        return_array = np.zeros(())
+        return_array = np.array([])
         # find missing duration
-        missing_duration = self.get_property('duration',
-                                             **context_and_values) -  sum(
-                                    s.get_property('duration', **context_and_values) for s in self._segments if s._properties['duration'] is not None)
+        missing_duration = own_duration - sum(s.get('duration', **context) for s in self._segments if not s.is_expandable)
+        if missing_duration < 0:
+            raise RuntimeError('The expandable element in this group cannot have negative length.')
         for s in self._segments:
-            if s._properties['duration'] is None:
+            if s.is_expandable:
                 # some unecessary copying here
-                return_array = np.append(return_array, s.forge(SR, **context_and_values, duration=missing_duration))
+                return_array = np.append(return_array, s.forge(SR, **context, duration=missing_duration))
             else:
                 # some unecessary copying here
-                nreturn_array = np.append(return_array, s.forge(SR, **context_and_values))
+                return_array = np.append(return_array, s.forge(SR, **context))
         return return_array
