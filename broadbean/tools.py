4bb0ad91484e89438064b7a3718bc4c7526683e5
Dominik Vogel <a-dovoge@microsoft.com>
2018-06-27 10:07:30 +0200

    zero and one based index for forged sequence

--- broadbean/tools.py
+++ broadbean/tools.py
@@ -25,6 +25,8 @@ def forged_sequence_dict_to_list(seq):
         return seq
     # top level transformation (no subsequnces)
     length = len(seq.keys())
+    # acommodate for 1 and 0 based indeces
+    min_val = min(seq.keys())
     list_of_elements = []
     for index in range(length):
         try:
@@ -35,7 +37,7 @@ def forged_sequence_dict_to_list(seq):
             # In general one could say we want to copy all, except for the
             # lowest level values, i.e. we want to change the structure but
             # not the content
-            list_of_elements.append(copy(seq[index]))
+            list_of_elements.append(copy(seq[index + min_val]))
         except KeyError:
             log.error(f'Error converting forged sequence dict to list: The '
                       f'sequence seems to be sparse. The element no. {index} '
@@ -47,6 +49,8 @@ def forged_sequence_dict_to_list(seq):
     for i_elem, elem in enumerate(return_val):
         dict_content = elem['content']
         length = len(dict_content.keys())
+        # acommodate for 1 and 0 based indeces
+        min_val = min(seq.keys())
         list_of_elements = []
         for index in range(length):
             try:
@@ -57,7 +61,7 @@ def forged_sequence_dict_to_list(seq):
                 # when using :meth:`broadbean.routing.route` the value of
                 # `data` is changed (namely by replacing it with another dict).
                 # if we don't copy here also the original dict will be changed
-                list_of_elements.append(copy(dict_content[index]))
+                list_of_elements.append(copy(dict_content[min_val + index]))
             except KeyError:
                 log.error(f'Error converting forged sequence dict to list: '
                           f'The subsequence seems to be sparse (Element no. '
