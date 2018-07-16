727279ae8e097d2d57498c68b5155eeac0f28680
Dominik Vogel <a-dovoge@microsoft.com>
2018-07-16 18:00:32 +0200

    add declarative linear transformation

--- broadbean/transformations.py
+++ broadbean/transformations.py
@@ -1,9 +1,24 @@
 from copy import copy
 
+from typing import Tuple, List
+from .types import Number, Symbol
+
 def get_transformed_context(input_context,
                             transformation: callable):
     new_context = copy(input_context)
     if transformation:
         transformation(new_context)
     return new_context
+
+
+def make_delcarative_linear_transformation(data: List[
+        Tuple[Symbol, List[Tuple[Number,Symbol]]]]):
+    def declarative_linear_transformation(context):
+        for item in data:
+            assigned = item[0]
+            value = sum([multiplier*context[symbol]
+                         for multiplier, symbol in item[1]])
+            context[assigned] = value
+    return declarative_linear_transformation
+
 
