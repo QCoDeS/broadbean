e5ebcd747857a9ab0c497567e6a6059510d9f4e2
WilliamHPNielsen <whpn@mailbox.org>
2018-09-19 11:40:14 +0200

    Pin matplotlib version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     # We might as well require what we know will work
     # although older numpy and matplotlib version will probably work too
     install_requires=['numpy>=1.12.1',
-                      'matplotlib>=2.0.1',
+                      'matplotlib==2.2.3',
                       'schema'],
 
     author='William H.P. Nielsen',
