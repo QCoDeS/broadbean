f33302cec6eb3b370a2968068c7798ec773088ad
Mikhail Astafev <astafan8@gmail.com>
2022-05-24 10:47:47 +0200

    mark broadbean as working on python 3.7-3.10

--- setup.py
+++ setup.py
@@ -25,7 +25,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10'
         ],
 
     keywords='Pulsebuilding signal processing arbitrary waveforms',
