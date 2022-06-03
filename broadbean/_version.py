5ccbee2d88f33ba97be8abcc0f0939cb52165207
Trevor Morgan <trmorgan@microsoft.com>
2022-06-02 23:07:08 -0700

    cleaning up build to be updated to current pep standards

---
+++ broadbean/_version.py
@@ -0,0 +1,12 @@
+def _get_version() -> str:
+    from pathlib import Path
+    import versioningit
+    import broadbean
+
+    project_dir = Path(qdwsdk.__file__).parent.parent
+    if not (project_dir / "pyproject.toml").exists():
+        project_dir = project_dir.parent
+    return versioningit.get_version(project_dir=project_dir)
+
+
+__version__ = _get_version()
--- setup.py
+++ setup.py
@@ -1,40 +1,7 @@
 from setuptools import setup
+from versioningit import get_cmdclasses
 
-setup(
-    name='broadbean',
-    version='0.10.0',
-
-    # We might as well require what we know will work
-    # although older numpy and matplotlib version will probably work too
-    install_requires=['numpy>=1.12.1',
-                      'matplotlib',
-                      'schema'],
-
-    author='William H.P. Nielsen',
-    author_email='William.Nielsen@microsoft.com',
-
-    description=("Package for easily generating and manipulating signal "
-                 "pulses. Developed for use with qubits in the quantum "
-                 "computing labs of Copenhagen, Delft, and Sydney, but "
-                 "should be generally useable."),
-
-    license='MIT',
-
-    packages=['broadbean'],
-
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Science/Research',
-        'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10'
-        ],
-
-    python_requires=">=3.7",
-
-    keywords='Pulsebuilding signal processing arbitrary waveforms',
-
-    url='https://github.com/QCoDeS/broadbean'
+if __name__ == "__main__":
+    setup(
+        cmdclass=get_cmdclasses(),
     )
